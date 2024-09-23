341d6a5f6c3190b4423e8b01c99836680940f35f
jorgemar73 <jorge_mar73@hotmail.com>
2024-09-23 12:14:21 -0500

    Primer commit de la pagina web de la inmobiliaria version sencilla

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+
+function loadPage(page) {
+    fetch(`pages/${page}.html`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.log("Error loading page:", error));
+}
+
+function loadService(service) {
+    fetch(`pages/${service}.html`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.log("Error loading page:", error));
+}
