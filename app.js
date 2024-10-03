277ad621d33f6b252bbfd1ddee36bda5bf4a5dce
jorgemar73 <jorge_mar73@hotmail.com>
2024-10-03 18:13:27 -0500

    Habilitación del formulario de contacto (frontend) y en la barra de menú

--- app.js
+++ app.js
@@ -17,3 +17,36 @@ function loadPage(page) {
         })
         .catch(error => console.log("Error loading page:", error));
 } */
+
+// app.js
+
+// Cargar inmuebles desde el servidor
+/* function loadInmuebles() {
+    fetch('http://localhost:3000/inmuebles')
+        .then(response => response.json())
+        .then(data => {
+            const cardsContainer = document.getElementById('cards-section .row');
+            cardsContainer.innerHTML = ''; // Limpiar el contenido anterior
+
+            data.forEach(inmueble => {
+                const cardHTML = `
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img src="${inmueble.imagen}" class="card-img-top" alt="${inmueble.titulo}">
+                            <div class="card-body">
+                                <h5 class="card-title">${inmueble.titulo}</h5>
+                                <p class="card-text">${inmueble.descripcion}</p>
+                                <p class="card-text">Precio: ${inmueble.precio}</p>
+                                <a href="#" class="btn btn-primary">Ver Detalles</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                cardsContainer.innerHTML += cardHTML;
+            });
+        })
+        .catch(error => console.error('Error cargando inmuebles:', error));
+}
+
+window.onload = loadInmuebles; */
+
